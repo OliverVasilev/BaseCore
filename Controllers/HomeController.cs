03b034f8dbf34e5a2d9ca4988d969d5c54e76334
Oliver Vasilev <oliver_vasilev@outlook.com>
2020-06-12 11:41:43 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestStore.Models;
+
+namespace TestStore.Controllers
+{
+    public class HomeController : Controller
+    {        //private readonly ILogger<HomeController> _logger;
+
+        private DataContext context;
+        public HomeController(DataContext ctx)
+        {
+            context = ctx;
+        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IActionResult Index()
+        {
+            return View(context.Products.First());
+        }
+
+    }
+}
